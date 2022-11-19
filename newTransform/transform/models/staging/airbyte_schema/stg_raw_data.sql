ba009f0c963f8c74127f4624e8a080b6422f3a4a
arodriguez2018 <artrod84@gmail.com>
2022-11-19 09:58:14 -0600

    delete new script

--- newTransform/transform/models/staging/airbyte_schema/stg_raw_data.sql
+++ newTransform/transform/models/staging/airbyte_schema/stg_raw_data.sql
@@ -1,4 +1,11 @@
 select 
-    *
+    round(low,2) as low, 
+    round(high,2) as high,
+    round(open,2) as open,
+    round(close,2) as close, 
+    volume, 
+    timestamp::date as timestamp_date, 
+    _airbyte_emitted_at,
+    _airbyte_ab_id
 from {{ source('airbyte_schema', 'raw_data')}}
-limit 9
+order by timestamp asc
